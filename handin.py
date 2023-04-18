ea56bc53badd7616ac92e228860ec229f333e3f4
Ben Kunkle <ben.kunkle@gmail.com>
2023-04-18 10:53:33 -0700

    assignment submission (not fully tested!)

--- handin.py
+++ handin.py
@@ -23,8 +23,12 @@ options.add_argument(
     "--list", "-l", action="store_true", help="List active assignments."
 )
 options.add_argument(
-        "--update", "-u", dest="update_certs", action="store_true", help="Download server certifications file (.pem file). Note: this file is required for the handin client to work. "
-        )
+    "--update",
+    "-u",
+    dest="update_certs",
+    action="store_true",
+    help="Download server certifications file (.pem file). Note: this file is required for the handin client to work. ",
+)
 
 HOSTNAME = "handin-1.brinckerhoff.org"
 PORT = 7979
@@ -34,8 +38,10 @@ ROOT_CERTS = pathlib.Path(__file__).parent / "server-cert.pem"
 # TODO: replace 2234 with {quarter} to allow for future generations
 ROOT_CERTS_GH_LINK = "https://raw.githubusercontent.com/jbclements/racket-handin-client/master/2234-csc430-handin/server-cert.pem"
 
+
 def download_certs():
     import urllib.request
+
     urllib.request.urlretrieve(ROOT_CERTS_GH_LINK, ROOT_CERTS)
 
 
@@ -76,8 +82,13 @@ class Handin:
         if self.verbose:
             printf(*args, **kwargs)
 
-    def write(self, msg):
-        msg = (msg + "\n").encode("utf-8")
+    def write(self, msg, append_newline=True):
+        if not isinstance(msg, (str, bytes)):
+            msg = str(msg)
+        if isinstance(msg, str):
+            msg = msg.encode("utf-8")
+        if append_newline:
+            msg += b"\n"
         self.ssl_socket.send(msg)
 
     def read(self, len=1024, message=True, max_tries=10240):
@@ -115,6 +126,64 @@ class Handin:
         res = self.read()
         active_assignments = [a.strip('"') for a in res.strip().strip("()").split()]
         return active_assignments
+
+    def get_user_info(self, uname, pword):
+        msgs = [
+            "get-user-fields",
+        ]
+        for msg in msgs:
+            self.write(msg)
+        return self.read()
+
+    def ensure_ok(self, src, msg=None, expected="ok"):
+        if msg is None:
+            msg = self.read()
+        assert msg == expected, f"{src} failed! Reason: {msg}"
+
+    def submit(self, uname, pword, asgn, file):
+        def wrap_quote(s):
+            return '"' + s + '"'
+
+        msgs = [
+            "set",
+            "username/s",
+            wrap_quote(uname),
+            "set",
+            "password",
+            wrap_quote(pword),
+            "set",
+            "assignment",
+            wrap_quote(asgn),
+            "save-submission",
+        ]
+        for s in msgs:
+            self.write(s)
+        self.ensure_ok("login")
+        with open(file, "rb") as submission:
+            content = submission.read()
+        self.write(len(content))
+        self.ensure_ok("upload", expected="go")
+        self.write("$", append_newline=False)
+        print("printing contents of", asgn, "type:", type(content))
+        self.write(content)
+
+        def read_messages():
+            match (msg := self.read()):
+                case "message":
+                    print("message:\n", self.read())
+                    read_messages()
+                case "message-final":
+                    print("message-final:\n", self.read())
+                    read_messages()
+                case "message-box":
+                    print("message-box:\n", self.read(), "\n", self.read())
+                    self.write(input("\nresponse:"))
+                    read_messages()
+                case _:
+                    return msg
+        self.ensure_ok("submit", msg=read_messages(), expected="confirm")
+        self.write("check")
+        self.ensure_ok("check", msg=read_messages(), expected="ok")
 
 
 if __name__ == "__main__":
